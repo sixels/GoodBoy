8fc18971a8a448beba4061ed4456a1d4a212ddfc
Sixels <sixels@protonmail.com>
2022-02-13 11:36:39 -0300

    Use  crate to handle time on wasm and native

--- src/utils.rs
+++ src/utils.rs
@@ -1,90 +1,44 @@
+#![allow(dead_code)]
+
 use std::future::Future;
 
-#[cfg(not(target_arch = "wasm32"))]
-use std::time::{self, Instant};
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
+use instant::{Duration, Instant};
 
-trait TimeUnit<T> {
-    fn now() -> Self
-    where
-        Self: Sized;
+pub struct Fps {
+    fps: usize,
+    last_fps: usize,
 
-    fn one_sec() -> T
-    where
-        Self: Sized;
+    start: Instant,
 }
 
-#[cfg(target_arch = "wasm32")]
-type Time = f64;
-#[cfg(not(target_arch = "wasm32"))]
-// type Time = Instant;
-#[cfg(target_arch = "wasm32")]
-#[wasm_bindgen]
-extern "C" {
-    type Date;
+impl Fps {
+    pub fn update(&mut self) -> usize {
+        let now = Instant::now();
+        if now > self.start + Duration::from_secs(1) {
+            self.last_fps = self.fps;
+            self.fps = 0;
 
-    #[wasm_bindgen(static_method_of = Date)]
-    pub fn now() -> f64;
-}
+            self.start = now;
+        }
 
-#[cfg(target_arch = "wasm32")]
-impl TimeUnit<f64> for f64 {
-    fn now() -> f64 {
-        Date::now()
+        self.fps += 1;
+        self.last_fps
     }
 
-    fn one_sec() -> f64 {
-        1000.0
+    pub fn current_rate(&self) -> usize {
+        self.last_fps
     }
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-impl TimeUnit<time::Duration> for Instant {
-    fn now() -> Self {
-        Instant::now()
-    }
-
-    fn one_sec() -> time::Duration {
-        time::Duration::from_secs(1)
+impl Default for Fps {
+    fn default() -> Self {
+        Self {
+            fps: Default::default(),
+            last_fps: Default::default(),
+            start: Instant::now(),
+        }
     }
 }
-
-// pub struct Fps {
-//     fps: usize,
-//     last_fps: usize,
-
-//     start: Time,
-// }
-
-// impl Fps {
-//     pub fn update(&mut self) -> usize {
-//         let now = Time::now();
-//         if now > self.start + Time::one_sec() {
-//             self.last_fps = self.fps;
-//             self.fps = 0;
-
-//             self.start = now;
-//         }
-
-//         self.fps += 1;
-//         self.last_fps
-//     }
-
-//     pub fn current_rate(&self) -> usize {
-//         self.last_fps
-//     }
-// }
-
-// impl Default for Fps {
-//     fn default() -> Self {
-//         Self {
-//             fps: Default::default(),
-//             last_fps: Default::default(),
-//             start: Time::now(),
-//         }
-//     }
-// }
 
 #[cfg(target_arch = "wasm32")]
 pub fn spawn<F: Future<Output = ()> + 'static>(f: F) {
